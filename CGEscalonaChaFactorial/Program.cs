5d5429561a729fdee5831bd13361b0f91d9bb96d
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-05-12 10:56:28 -0600

    Add project files.

---
+++ CGEscalonaChaFactorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGEscalonaChaFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(suma(7));
+            Console.ReadLine();
+
+
+        }
+        public static int suma(int n)
+        {
+
+            if (n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n + suma(n - 1);
+            }
+
+
+        }
+    }
+}
